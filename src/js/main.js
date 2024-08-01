8d70898bf043dfe3fdaf4102e9b64b0f9a0814a7
svist1212 <37883847+svist1212@users.noreply.github.com>
2024-08-02 01:26:55 +0400

    Updeted hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import '../assets/scss/main.scss';
+
+//componets
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();
