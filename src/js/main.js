71eeea37c4aa044e1df0e21afdba57a84ed50dde
svist1212 <37883847+svist1212@users.noreply.github.com>
2024-08-25 01:58:37 +0400

    Created insight and slider

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
